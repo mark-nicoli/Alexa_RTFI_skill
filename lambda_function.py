d8756c84756f3df5eb91171ff90a4e88b1234686
mark-nicoli <mark21198@gmail.com>
2019-11-05 14:34:20 +0000

    updated lambda functions. now using ask_sdk

--- lambda_function.py
+++ lambda_function.py
@@ -1,86 +1,163 @@
-from __future__ import print_function
-from botocore.vendored import requests
-from datetime import datetime
 import logging
+import ask_sdk_core.utils as ask_utils
 
-def lambda_handler(event, context):
-    if event['request']['type'] == "LaunchRequest":
-        return get_help() #provide functionality desc
-    elif event['request']['type'] == "IntentRequest":
-        return on_intent(event) #execute command
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+from ask_sdk_core.handler_input import HandlerInput
 
-def on_intent(event):
-    intent = event['request']['intent']
-    intent_name = event['request']['intent']['name']
-    print('on_intent, session: ')
-    print(event['session'])
+from ask_sdk_model import Response
 
-    if intent_name == "GetTrainTimes":
-        return get_train_time(event)
-    elif intent_name == "getBusTimes":
-        return get_bus_time(event)
-    elif intent_name == "AMAZON.HelpIntent":
-        return get_help()
-    elif intent_name == "AMAZON.CancelIntent":
-        return nothing()
-    elif intent_name == "AMAZON.StopIntent":
-        return nothing()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-def get_train_time(event):
-    train_times = IrishRailRTPI()
-    origin = input('origin: ')
-    #destination = raw_input('destination: ')
-    dir = input('direction: ')
-    data = json.dumps(train_times.get_station_by_name(origin,num_minutes=30), indent=4, sort_keys=True)
-    resp = json.loads(data)
 
-    for i in range(len(resp)):
-        dict_data = resp[i]
-        if dict_data['direction']==dir: #filter out by direction
-            #print(dict_data)
-            to_say = "the next "+dir+" train is in"+dict_data['due_in_mins']+"mins"
-            return say_duration(to_say)
+class LaunchRequestHandler(AbstractRequestHandler):
+    """Handler for Skill Launch."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
 
-def get_bus_time(event):
-    route = input('Enter route number: ')
-    stop_number = input('Enter stop number: ')
-    g = db.RtpiApi(user_agent='test')
-    bus_times = g.rtpi(stop_number, route)
-    #print(my_stop.timestamp)
-    to_say = "the next"+route+"bus is in in"+bus_times.results[0]['duetime']+"mins"
-    return say_duration(to_say)
+        return ask_utils.is_request_type("LaunchRequest")(handler_input)
 
-def build_speechlet_response(title, output, reprompt_text, shouldEndSession):
-    return {
-        'outputSpeech': {
-            'type': 'PlainText',
-            'text': output
-        },
-        'card': {
-            'type': 'Simple',
-            'title': title,
-            'content': output
-        },
-        'reprompt': {
-            'outputSpeech': {
-                'type': 'PlainText',
-                'text': reprompt_text
-            }
-        },
-        'shouldEndSession': should_end_session
-    }
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "Welcome, you can ask for train or bus times. Which would you like to try?"
 
-def nothing():
-    retutn(build_response({}))
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask(speak_output)
+                .response
+        )
 
-def get_help():
-    speech_output = "welcome to transport times"
-    card_title = "transport times error"
-    shouldEndSession = False
-    return build_response(build_speechlet_response(card_title, speed_output, None, shouldEndSession))
 
-def say_duration(duration):
-    speech_output = duration
-    card_title = 'Transport times'
-    should_end_session = True
-    return build_response(build_speechlet_response(card_title, speech_output, None, should_end_session))
+class GetTrainTimesHandler(AbstractRequestHandler):
+    """Handler for Hello World Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("GetTrainTimes")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "you have opened train times"
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                # .ask("add a reprompt if you want to keep the session open for the user to respond")
+                .response
+        )
+
+class HelpIntentHandler(AbstractRequestHandler):
+    """Handler for Help Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("AMAZON.HelpIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "You can say hello to me! How can I help?"
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask(speak_output)
+                .response
+        )
+
+
+class CancelOrStopIntentHandler(AbstractRequestHandler):
+    """Single handler for Cancel and Stop Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
+                ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input))
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "Goodbye!"
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .response
+        )
+
+
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    """Handler for Session End."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+
+        # Any cleanup logic goes here.
+
+        return handler_input.response_builder.response
+
+
+class IntentReflectorHandler(AbstractRequestHandler):
+    """The intent reflector is used for interaction model testing and debugging.
+    It will simply repeat the intent the user said. You can create custom handlers
+    for your intents by defining them above, then also adding them to the request
+    handler chain below.
+    """
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("IntentRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        intent_name = ask_utils.get_intent_name(handler_input)
+        speak_output = "You just triggered " + intent_name + "."
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                # .ask("add a reprompt if you want to keep the session open for the user to respond")
+                .response
+        )
+
+
+class CatchAllExceptionHandler(AbstractExceptionHandler):
+    """Generic error handling to capture any syntax or routing errors. If you receive an error
+    stating the request handler chain is not found, you have not implemented a handler for
+    the intent being invoked or included it in the skill builder below.
+    """
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
+        return True
+
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        logger.error(exception, exc_info=True)
+
+        speak_output = "Sorry, I had trouble doing what you asked. Please try again."
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask(speak_output)
+                .response
+        )
+
+# The SkillBuilder object acts as the entry point for your skill, routing all request and response
+# payloads to the handlers above. Make sure any new handlers or interceptors you've
+# defined are included below. The order matters - they're processed top to bottom.
+
+
+sb = SkillBuilder()
+
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(GetTrainTimesHandler())
+#sb.add_request_handler(GetBusTimesHandler())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(CancelOrStopIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+
+sb.add_exception_handler(CatchAllExceptionHandler())
+
+lambda_handler = sb.lambda_handler()

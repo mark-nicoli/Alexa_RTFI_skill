35473521745bf66ef67588c2576184ec145ca879
mark-nicoli <mark21198@gmail.com>
2019-11-19 15:15:44 +0000

    update

--- lambda_function.py
+++ lambda_function.py
@@ -181,7 +181,7 @@ def lambda_handler(event, context):
     # if (event['session']['application']['applicationId'] !=
     #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
     #     raise ValueError("Invalid Application ID")
-
+    
     if event['session']['new']:
         on_session_started({'requestId': event['request']['requestId']},
                            event['session'])
@@ -191,4 +191,4 @@ def lambda_handler(event, context):
     elif event['request']['type'] == "IntentRequest":
         return on_intent(event['request'], event['session'])
     elif event['request']['type'] == "SessionEndedRequest":
-        return on_session_ended(event['request'], event['session'])(event['request'], event['session'])
+        return on_session_ended(event['request'], event['session'])x(event['request'], event['session'])

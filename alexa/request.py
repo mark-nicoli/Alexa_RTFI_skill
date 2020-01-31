b9eeb5cc4035fe438fcce73132983f152d274232
mark-nicoli <mark21198@gmail.com>
2020-01-31 14:46:14 +0000

    update

--- alexa/request.py
+++ alexa/request.py
@@ -40,7 +40,6 @@ def rail_time():
         dict_data = resp[i]
         if dict_data['destination'].lower()==destination: #filter out by origin and make into lower case for alexa
             print(dict_data['destination'].lower())  # visualization of json data
-            print(destinations)
             '''
                 origin = coolmine.
                 expected_arrival_time = time train arrives at coolmine
